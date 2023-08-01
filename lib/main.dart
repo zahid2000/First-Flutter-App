8af26a32c09989c1ee0186976b58074bb72df70f
zahid2000 <zahidmamedov07@gmail.com>
2023-08-01 16:20:03 +0400

    First flutter app

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(title: const Text("Öğrenci takip sistemi"),),
+      body:const Center(child:  Text("Hello World")));
+  }
+}
